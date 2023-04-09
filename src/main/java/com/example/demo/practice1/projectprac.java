dd3d052dbe8898df5a469895015347f7ce17c239
a1 <jennykim6890@naver.com>
2023-04-09 12:58:09 +0900

    feat : 연습

--- src/main/java/com/example/demo/practice1/projectprac.java
+++ src/main/java/com/example/demo/practice1/projectprac.java
@@ -4,8 +4,9 @@ public class projectprac {
     private final Dto dto = new Dto();
     public static void main(String[] args) {
 
-
-
-
+        System.out.println("영석이다");
+        System.out.println("영석이다1");
+        System.out.println("영석이다2");
+        System.out.println("영석이다3");
     }
 }
